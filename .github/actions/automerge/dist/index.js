1e4362adc3a1fe6f394b1ba8d31be1ee927febd4
Roshan Jossy <rjossy@goodgamestudios.com>
2020-05-06 19:41:39 +0200

    update text

--- .github/actions/automerge/dist/index.js
+++ .github/actions/automerge/dist/index.js
@@ -25378,9 +25378,9 @@ async function run() {
         **Next steps**
         - Continue contributing: If you're looking for projects to contribute to, checkout our [webapp](https://firstcontributions.github.io).
         - Join our slack group: We have a community to help/support contributors. [Join slack group](https://join.slack.com/t/firstcontributors/shared_invite/enQtNjkxNzQwNzA2MTMwLTVhMWJjNjg2ODRlNWZhNjIzYjgwNDIyZWYwZjhjYTQ4OTBjMWM0MmFhZDUxNzBiYzczMGNiYzcxNjkzZDZlMDM).
-        - Share on social media: You can share this content to help more people [share](https://firstcontributions.github.io/#social-share).
+        - Share on social media: You can share this content to help more people. [Share](https://firstcontributions.github.io/#social-share).
 
-        We'd love to hear your thoughts about this project. Let us know how we can improve my commenting or opening an issue here.`;
+We'd love to hear your thoughts about this project. Let us know how we can improve my commenting or opening an issue here.`;
 
     // Create a comment on pull request
     const response = await octokit.issues.createComment({
